a8be8b14411cd54bc34a5bd24afe325bab9b4f35
YanVictoria <yannvictoria05@gmail.com>
2021-07-30 13:33:09 +0800

    Add js file for copy to clipboard function

---
+++ javascript/copy_func.js
@@ -0,0 +1,11 @@
+  function copyFunc(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+    
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+    
+    alert("Copied to clipboard: " + TempText.value);
+  }
