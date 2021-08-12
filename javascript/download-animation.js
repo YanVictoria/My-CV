01583e76281ade068447929a388c03b4035dc430
YanVictoria <yannvictoria05@gmail.com>
2021-08-12 16:46:07 +0800

    Add js file for animation

---
+++ javascript/download-animation.js
@@ -0,0 +1,7 @@
+
+
+      const animationContainer = document.createElement("div");
+      animationContainer.classList.add("animation-container")
+      animationContainer.innerHTML = "Download PDF here:";
+      document.querySelector(".fixed").append(animationContainer);
+      
